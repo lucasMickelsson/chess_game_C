e2a768acca5f87bfd4f9a81d8cf1d6f34d77e508
Lucas Mickelsson <lucas.mickelsson@ha.ax>
2022-03-29 19:00:01 +0300

    added chess logo and confirm message

--- src/ChessGame/main.c
+++ src/ChessGame/main.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <conio.h>
+
+void printLines(void);
+
+void printLines(void)
+{
+    printf("\n");
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 45; j++)
+        {
+            printf("-");
+        }
+        printf("\n");
+    }
+}
+
+void printLogo(void)
+{
+    printLines();
+    printf("    _____ _    _ ______  _____ _____\n");
+    printf("   / ____| |  | |  ____|/ ____/ ____|\n");
+    printf("  | |    | |__| | |__  | (___| (___ \n");
+    printf("  | |    |  __  |  __|  \\___ \\\\___ \\ \n");
+    printf("  | |____| |  | | |____ ____) |___) |\n");
+    printf("   \\_____|_|  |_|______|_____/_____/\n\n");
+    printLines();
+}
+void printWelcomeMessage(void)
+{
+    printf("    Welcome to chess game by Lucas Mickelsson\n");
+    printf("           'press enter to continue'");
+}
 
 int main(void)
 {
+    // bool exitProgram = false;
+    int confirm;
+    do
+    {
+        printLogo();
+        printWelcomeMessage();
+        confirm = getch();
+    } while (confirm != 13);
+
+    // printLogo();
 }
