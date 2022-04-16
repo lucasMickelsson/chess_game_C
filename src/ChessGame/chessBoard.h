aa9dc7a6c9e2210611953f7e138d67ff24311f98
Lucas Mickelsson <lucas.mickelsson@ha.ax>
2022-04-16 15:39:03 +0300

    added documentation for chessboard functions

--- src/ChessGame/chessBoard.h
+++ src/ChessGame/chessBoard.h
@@ -31,17 +31,50 @@ void initChessBoard(char board[8][8]);
  */
 void printBoard(char board[8][8]);
 
+/**
+ * @brief Get the Piece At Position on the chess board
+ *
+ * @param board chess board array
+ * @param row
+ * @param col
+ * @return the pieceid back
+ */
 char getPieceAtPosition(char board[8][8], int row, int col);
+
+/**
+ * Function for printing a pieceString on the board
+ *
+ * @param pieceid id for the piece to be printed
+ */
 void printPieceStrings(char pieceId);
+
+/**
+ * Function for printing lines for chessboard
+ */
 void printChessBoardLines();
+
+/**
+ * Function for changing position between to pieces
+ * @param board
+ * @param oldRow
+ * @param oldCol
+ * @param newRow
+ * @param newCol
+ */
 void changeBoard(char board[8][8], int oldRow, int oldCol, int newRow, int newCol);
+
+/**
+ * Function for checking if a string is a chess coordinate
+ * @param chessMove
+ * @return true if its valid false otherwise
+ */
 bool positionStrings(char *chessMove);
 
 /**
- * @brief Get the Coords object
+ * @brief Get the chess coordinates for a chess move for example A1= 0 0
  *
- * @param string1
- * @return struct chessCoord
+ * @param string1 the chessmove example B2 A4 B6
+ * @return struct with 2 integer for col and row
  */
 struct Coord getCoords(char *string1);
 #endif
